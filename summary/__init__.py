9c0f0a1da1d37e4ffdaba0cb1ae45acbe724f34f
Jaden505 <jadenxxxl@gmail.com>
2024-10-23 16:47:38 +0200

    fix request method confusion now GET

--- summary/__init__.py
+++ summary/__init__.py
@@ -10,22 +10,26 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     apq_id = req.params.get('apq_id')
 
     if apq_id:
-        df_ideas,question = get_ideas(apq_id)
-        
-        df_ideas_anonymized = anonymize_text_data(df_ideas, 'idea')
-        summary = get_summary(df_ideas_anonymized, apq_id,question)
-        
-        if not summary:
-            return func.HttpResponse(f"None",status_code=409)
-        else:
-            insert_in_db(summary, apq_id)    
+        try:
+            df_ideas, question = get_ideas(apq_id)
+            df_ideas_anonymized = anonymize_text_data(df_ideas, 'idea')
+            summary = get_summary(df_ideas_anonymized, apq_id, question)
+            
+            if not summary:
+                return func.HttpResponse("None", status_code=409)
+            else:
+                insert_in_db(summary, apq_id)
+                return func.HttpResponse(summary, status_code=200)
+
+        except Exception as e:
+            logging.error(f"Error processing the request: {e}")
             return func.HttpResponse(
-                summary,
-                status_code=200
+                f"Server Error: An error occurred while processing the request. error: {e}",
+                status_code=500
             )
       
     else:
         return func.HttpResponse(
-             "No version or apq_id found",
-             status_code=200
+            "No version or apq_id found",
+            status_code=400
         )
